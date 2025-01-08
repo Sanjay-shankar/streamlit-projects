54e8b716029d3e7303475e6ff3d4dc754c0f838b
Shankar-Rajan <sanjay16shankar@gmail.com>
2025-01-08 19:23:57 +0530

    Modified

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
 import streamlit as st
-from langchain.llms import OpenAI  # Or ChatOpenAI, based on your version
+from langchain_openai.chat_models import ChatOpenAI
 
-st.title("Sample App")
+st.title("🦜🔗 Quickstart App")
 
 # Retrieve the OpenAI API key from Streamlit secrets
 openai_api_key = st.secrets["openai"]["api_key"]
 
+
 def generate_response(input_text):
-    if openai_api_key.startswith('sk-'):
-        llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
-        response = llm(input_text)
-        st.info(response)
-    else:
-        st.error("Invalid OpenAI API Key")
+    model = ChatOpenAI(temperature=0.7, api_key=openai_api_key)
+    st.info(model.invoke(input_text))
+
 
 with st.form("my_form"):
-    text = st.text_area("Enter text:", "What are ----")
+    text = st.text_area(
+        "Enter text:",
+        "What are the three key pieces of advice for learning how to code?",
+    )
     submitted = st.form_submit_button("Submit")
-
-    if submitted:
+    if not openai_api_key.startswith("sk-"):
+        st.warning("Please enter your OpenAI API key!", icon="⚠")
+    if submitted and openai_api_key.startswith("sk-"):
         generate_response(text)
